3d866fb6189f10fd022c379c9c1637bbdf47ccc1
felipeprata10 <felipeprata10@gmail.com>
2025-11-14 22:56:41 -0300

    Inicio

---
+++ app/src/main/java/com/example/myapplication/Alunos.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import javax.xml.namespace.QName;
+
+public class Alunos {
+    private int id;
+    private String name;
+    private String documento;
+    private String curso;
+
+    public Alunos(int id, String doc, String course){
+        this.id = id;
+        this.id = name;
+        this.id = doc;
+        this.id = course;
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getDocumento(){
+        return documento;
+    }
+
+    public String getCurso(){
+        return curso;
+    }
+
+}
